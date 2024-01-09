56381023ac8d3b6382237bbe6747353744469fd4
Sihwan Kim <vkn123@naver.com>
2024-01-09 09:20:00 +0900

    Init Spring Project

---
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/VoteshareApplication.kt
@@ -0,0 +1,11 @@
+package com.van1164.voteshare
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoteshareApplication
+
+fun main(args: Array<String>) {
+	runApplication<VoteshareApplication>(*args)
+}
---
+++ BE/voteshare/src/test/kotlin/com/van1164/voteshare/VoteshareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.van1164.voteshare
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VoteshareApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
