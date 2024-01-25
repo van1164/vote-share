b8ebf94a2c1c6a4712bbf1267ac000c2d3144168
van1164 <vkn123@naver.com>
2024-01-25 20:27:13 +0900

    feat: vote Controller 주소 변경

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/VoteController.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/VoteController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*
 
 
 @RestController
-@RequestMapping("/vote")
+@RequestMapping("api/vote")
 class VoteController(
     val voteService: VoteService,
     val questionService: QuestionService,
