69439e0120d53cd5ba4d9c001108961eb3158991
van1164 <vkn123@naver.com>
2024-01-25 20:25:51 +0900

    chore: swagger 설정 변경

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/swagger/SwaggerConfig.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/swagger/SwaggerConfig.kt
@@ -1,12 +1,44 @@
 package com.van1164.voteshare.config.swagger
 
+import com.van1164.voteshare.JwtTokenProvider
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 
+@OpenAPIDefinition(
+    info = Info(
+        title = "Vote-Share API 명세서",
+        description = "Vote-Share에 사용되는 API 명세서",
+        version = "v1"
+    )
+)
 @Configuration
-@EnableWebMvc
 class SwaggerConfig {
 
+    private val BEARER_TOKEN_PREFIX = "Bearer";
+
+    @Bean
+    fun openAPI() : OpenAPI {
+        val jwtSchemeName = BEARER_TOKEN_PREFIX;
+        val securityRequirement = SecurityRequirement().addList(jwtSchemeName);
+        val components =  Components()
+            .addSecuritySchemes(jwtSchemeName, SecurityScheme()
+                .name(jwtSchemeName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER_TOKEN_PREFIX)
+                .bearerFormat(BEARER_TOKEN_PREFIX));
+
+        // Swagger UI 접속 후, 딱 한 번만 accessToken을 입력해주면 모든 API에 토큰 인증 작업이 적용됩니다.
+        return OpenAPI()
+            .addSecurityItem(securityRequirement)
+            .components(components);
+    }
 }
--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/dto/VoteDTO.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/dto/VoteDTO.kt
@@ -1,13 +1,19 @@
 package com.van1164.voteshare.dto
 
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
 
 
 data class VoteDTO(
-        val title: String,
-        val subTitle : String,
-        @RequestPart val mainImage : MultipartFile?,
-        val publicShare : Boolean,
-        val questionList : MutableList<QuestionDTO>
-        )
+    @Parameter(name = "투표 제목", description = "투표 제목", example = "투표 제목", required = true)
+    val title: String,
+    val subTitle: String,
+    @Parameter(name = "메인 이미지", description = "투표 메인 이미지", required = true, content = [Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE)])
+    @RequestPart
+    val mainImage: MultipartFile?,
+    val publicShare: Boolean,
+    val questionList: MutableList<QuestionDTO>
+)
