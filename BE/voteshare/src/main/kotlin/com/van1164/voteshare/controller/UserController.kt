d005f3182638911fdd105a44b383aca1e13d4d61
van1164 <vkn123@naver.com>
2024-01-25 20:46:06 +0900

    feat: 로그인 성공시 엔티티 반환 구현

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/UserController.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/UserController.kt
@@ -1,10 +1,13 @@
 package com.van1164.voteshare.controller
 
 import com.van1164.voteshare.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
-@Controller
+@RestController
 @RequestMapping(value = ["/user"])
 class UserController(val userService: UserService) {
 
@@ -14,8 +17,8 @@ class UserController(val userService: UserService) {
     }
 
     @GetMapping("/login")
-    fun createUser(): String {
-        return "TEST"
+    fun createUser(): ResponseEntity<Any> {
+        return ResponseEntity("로그인성공", HttpStatus.OK)
     }
 
 }
