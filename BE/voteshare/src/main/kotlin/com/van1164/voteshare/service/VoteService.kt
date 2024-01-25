86adc58da40658871a3cee50553a7c0a6f379f3f
Sihwan Kim <vkn123@naver.com>
2024-01-25 14:09:54 +0900

    refactor: dateTime도 util에서 처리하게 구현

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/service/VoteService.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/service/VoteService.kt
@@ -4,7 +4,7 @@ import com.van1164.voteshare.domain.User
 import com.van1164.voteshare.domain.Vote
 import com.van1164.voteshare.dto.VoteDTO
 import com.van1164.voteshare.repository.VoteRepository
-import com.van1164.voteshare.util.UuidUtil
+import com.van1164.voteshare.util.ServiceUtil
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -18,13 +18,14 @@ import kotlin.collections.HashMap
 class VoteService(val voteRepository:VoteRepository,val questionService: QuestionService, val s3Service: S3Service) : BaseService() {
 
     suspend fun createVote(voteDTO: VoteDTO, user : User): Vote {
-        val profileImageUrl = s3Service.uploadImage(voteDTO.mainImage)
-        val voteUrl = UuidUtil().createUUID()
+        voteDTO.mainImage
+        val profileImageUrl = voteDTO.mainImage?.let { s3Service.uploadImage(it) }
+        val voteUrl = ServiceUtil.createUUID()
         val title = voteDTO.title
         val subTitle = voteDTO.subTitle
-        val createDate = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())
-        val updatedDate = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())
-        val vote = Vote(title, subTitle, voteUrl, createDate, updatedDate,  user)
+        val createDate = ServiceUtil.dateTimeNow()
+        val updatedDate = ServiceUtil.dateTimeNow()
+        val vote = Vote(title, subTitle, voteUrl, createDate, updatedDate,  user, mainImageUrl =  profileImageUrl)
         tx.begin()
         voteRepository.save(vote)
         tx.commit()
