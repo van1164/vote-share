44a162db769710eaba139508f2986aa9fa24482b
Sihwan Kim <vkn123@naver.com>
2024-01-26 13:07:32 +0900

    feat : response 생성자 어노테이션 추가

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/dto/PopularVoteResponseDTO.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/dto/PopularVoteResponseDTO.kt
@@ -3,8 +3,12 @@ package com.van1164.voteshare.dto
 import com.van1164.voteshare.domain.Question
 import com.van1164.voteshare.domain.User
 import jakarta.persistence.*
+import lombok.AllArgsConstructor
+import lombok.NoArgsConstructor
 import java.util.*
 
+@NoArgsConstructor
+@AllArgsConstructor
 data class PopularVoteResponseDTO(
 
     val title : String,
