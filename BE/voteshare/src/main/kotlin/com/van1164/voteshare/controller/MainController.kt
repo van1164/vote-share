f2d2a1f958441887cf4f1c7ed3f6fdfbe8030166
van1164 <vkn123@naver.com>
2024-02-05 12:47:46 +0900

    description: 메인페이지 스웨거 추가

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/MainController.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/MainController.kt
@@ -1,7 +1,13 @@
 package com.van1164.voteshare.controller
 
+import com.van1164.voteshare.domain.User
+import com.van1164.voteshare.dto.PopularVoteResponseDTO
 import com.van1164.voteshare.service.UserService
 import com.van1164.voteshare.service.VoteService
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -16,6 +22,16 @@ import org.springframework.web.bind.annotation.RestController
 class MainController(val voteService : VoteService) {
 
 
+
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "게시글 조회 성공",
+                content = [Content(schema = Schema(implementation = PopularVoteResponseDTO::class))]
+            ),
+        ]
+    )
     @GetMapping("/main_page")
     fun mainPage() : ResponseEntity<Any> {
         return voteService.loadMainPageData()
