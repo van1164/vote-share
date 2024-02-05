3b875c9e265ff21c2a103f84a2baa7227b97e089
van1164 <vkn123@naver.com>
2024-02-05 12:44:56 +0900

    feat: 마이페이지 Swagger 코드 추가

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/UserController.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/controller/UserController.kt
@@ -1,7 +1,12 @@
 package com.van1164.voteshare.controller
 
+import com.van1164.voteshare.domain.User
 import com.van1164.voteshare.service.RedisService
 import com.van1164.voteshare.service.UserService
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
@@ -24,6 +29,15 @@ class UserController(val userService: UserService, val redisService: RedisServic
         return ResponseEntity("로그인성공", HttpStatus.OK)
     }
 
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "게시글 조회 성공",
+                content = [Content(schema = Schema(implementation = User::class))]
+            ),
+        ]
+    )
     @GetMapping("/mypage")
     fun getMyPage(
         @RequestHeader(value = "Authorization") token: String
