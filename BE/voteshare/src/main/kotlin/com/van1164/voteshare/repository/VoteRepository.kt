867be0bc3ce30034bacc6c5a67457be0a6b1e0d9
van1164 <vkn123@naver.com>
2024-02-19 14:26:12 +0900

    fix: 🌕 Popular Vote idList 파라미터 추가

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/repository/VoteRepository.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/repository/VoteRepository.kt
@@ -58,6 +58,6 @@ class VoteRepository : BaseRepository() {
 
     fun loadVoteListById(popularVoteIdList: List<Long?>): Any {
         val jpql = "select new com.van1164.voteshare.dto.VoteDetailDTO(v.title,v.subTitle,v.publicShare,v.maxSelectItem,v.allVoteSum,v.updatedDate,v.mainImageUrl) from Vote v where v.id in (:idList) order by v.allVoteSum desc "
-        return em.createQuery(jpql,VoteDetailDTO::class.java).resultList
+        return em.createQuery(jpql,VoteDetailDTO::class.java).setParameter("idList",popularVoteIdList) .resultList
     }
 }
