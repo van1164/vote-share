8c2f4590be69c49cef6b9504108fdf101299c72f
van1164 <vkn123@naver.com>
2024-02-19 13:23:04 +0900

    chore: ✈ 프론트 주소 변경

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/auth/CorsConfig.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/auth/CorsConfig.kt
@@ -13,7 +13,7 @@ class CorsConfig {
     fun corsConfigurationSource(): CorsConfigurationSource {
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.allowedOrigins = listOf("http://localhost:3000")
+        config.allowedOrigins = listOf("http://localhost:3000", "http://vote-share.o-r.kr", "http://13.57.22.174")
         config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         config.allowedHeaders = listOf("*")
         config.exposedHeaders = listOf("*")
