fec223c606c8c8d753e089557f47d77b0888d45d
van1164 <vkn123@naver.com>
2024-02-20 18:34:27 +0900

    chore: 🎈인증서 발급 sh 수정

--- BE/voteshare/init-letsencrypt.sh
+++ BE/voteshare/init-letsencrypt.sh
@@ -50,21 +50,21 @@ docker-compose run --rm --entrypoint "\
 echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
-
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
-
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+#echo "### Requesting Let's Encrypt certificate for $domains ..."
+##Join $domains to -d args
+#domain_args=""
+#for domain in "${domains[@]}"; do
+#  domain_args="$domain_args -d $domain"
+#done
+#
+## Select appropriate email arg
+#case "$email" in
+#  "") email_arg="--register-unsafely-without-email" ;;
+#  *) email_arg="--email $email" ;;
+#esac
+#
+## Enable staging mode if needed
+#if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
