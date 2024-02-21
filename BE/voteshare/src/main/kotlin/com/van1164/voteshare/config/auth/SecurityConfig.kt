fbaac03b3a550145305e2384574ce70cc71c6326
van1164 <vkn123@naver.com>
2024-02-21 10:28:00 +0900

    fix: 🎈디테일 페이지 다른 사용자 접근 하가 처리

--- BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/auth/SecurityConfig.kt
+++ BE/voteshare/src/main/kotlin/com/van1164/voteshare/config/auth/SecurityConfig.kt
@@ -25,6 +25,7 @@ class SecurityConfig(val oAuthSuccessHandler: OAuthSuccessHandler, val oAuthFail
             csrf { disable() }
             cors { }
             authorizeRequests {
+                authorize("/api/v1/vote/detail_page/**", permitAll)
                 authorize("/api/v1/main_page", permitAll)
                 authorize("/api/**", authenticated)
             }
